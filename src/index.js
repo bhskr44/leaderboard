bedf68641b7a5bdafbe0cf162188634137f55af6
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-11-19 03:30:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,16 +8,16 @@ import renderScores from './modules/renderScores.js';
 // created = createGame(new Game('My Game'))
 const form = document.querySelector('form');
 const refresh = document.querySelector('#refresh');
-
+renderScores();
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.querySelector('#name').value;
   const newScore = document.querySelector('#score').value;
-  let entry = new Scores(name, newScore);
+  const entry = new Scores(name, newScore);
   saveScores(entry);
   form.reset();
 });
 
 refresh.addEventListener('click', () => {
-    renderScores();
+  renderScores();
 });
